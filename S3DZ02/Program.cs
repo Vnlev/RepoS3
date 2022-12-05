ab504dc5fcf4fb96b2c2354d8e15835fde5428d6
Vladislav <vnl.i@ya.ru>
2022-12-05 22:51:58 +0300

    Решение ДЗ задача 2 семинар 3

---
+++ S3DZ02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2 Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int Promt(string messege)
+{
+    System.Console.Write($"{messege}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+}
+ 
+int Ax = Promt("Введите координату точки А по оси х");
+int Ay = Promt("Введите координату точки А по оси y");
+int Az = Promt("Введите координату точки А по оси z");
+int Bx = Promt("Введите координату точки B по оси х");
+int By = Promt("Введите координату точки B по оси y");
+int Bz = Promt("Введите координату точки B по оси z");
+double distanceAB = GetDistance(Ax, Ay, Az, Bx, By, Bz);
+System.Console.WriteLine($"Расстояние между точками А и В в 3D пространстве равно {distanceAB:f2}");
+
