611d848ccf0de25b86f59bcc8f3cacbf25d1f5f9
Vladislav <vnl.i@ya.ru>
2022-12-05 23:28:17 +0300

    Решение ДЗ задача 2 семинар 3

---
+++ S3DZ03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3 
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Promt(string messege)
+{
+    System.Console.Write($"{messege}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string GetCube(int n)
+{
+    string stringOfCube = "1";
+    for (int i = 2; i <= n; i++)
+    {
+        stringOfCube = stringOfCube + ", " + Convert.ToString(i * i * i);
+    }
+    return stringOfCube;
+}
+
+int N = Promt("Введите число");
+string Cube = GetCube( N );
+System.Console.WriteLine($"Кубы чисел от 1 до N {Cube}");
