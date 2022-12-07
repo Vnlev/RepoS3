468acfe882a3cfbc1dcb8b91d3df5f704ce09a20
Vladislav <vnl.i@ya.ru>
2022-12-07 22:46:29 +0300

    Решения ДЗ задача 2 семинар 4

---
+++ S4DZ02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу,которая принимает на вход число 
+// и выдаёт сумму цифр в числе. Реализовать через функции.
+
+int Prompt(string messege)      //Функция считывает число
+{
+    System.Console.Write($"{messege}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A = Prompt("Введите число");      //функ считывает и возвращаемое значение копируется в А
+int P = 1;
+int C = 0;
+while (P <= A)        //Цикл пока
+{
+    C = C + A/P %10; 
+    P = 10*P;
+}
+   System.Console.WriteLine(C);
