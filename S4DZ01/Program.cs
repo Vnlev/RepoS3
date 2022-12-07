a30a6166cac05dcdef0fcd0f8d9036ce7d5ba0e7
Vladislav <vnl.i@ya.ru>
2022-12-07 22:19:48 +0300

    Решения ДЗ задача 1 семинар 4

---
+++ S4DZ01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. 
+// Реализовать через функции.
+
+int Prompt(string messege)
+{
+    System.Console.Write($"{messege}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A = Prompt("Введите число");
+int B = Prompt("Введите число");
+int C = 1;
+for(int i = 0;i < B; i++) 
+{
+    C = C*A;
+}
+   System.Console.WriteLine(C);
